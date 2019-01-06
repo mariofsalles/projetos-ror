b397361eaa23ded8639840bcde4d3e07c40ac40a
Mario Salles <mariofsalles@gmail.com>
2019-01-06 15:59:13 +0000

    Creating Posts

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_06_154953) do
+ActiveRecord::Schema.define(version: 2019_01_06_155823) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
 end
