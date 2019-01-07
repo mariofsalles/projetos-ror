bf28b19108ad3eba18eb64cc19305adef7964dc3
Mario Salles <mariofsalles@gmail.com>
2019-01-07 09:58:42 +0000

    Foreignkey in Model: Contact and Kind

--- schedule/app/models/contact.rb
+++ schedule/app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  has_many :phones
+  has_one :address
+
+  has_many :contact_kinds
+  has_many :kinds, through: :contact_kinds
+
 end
--- schedule/app/models/kind.rb
+++ schedule/app/models/kind.rb
@@ -1,2 +1,4 @@
 class Kind < ApplicationRecord
+  has_many :contact_kinds
+  has_many :contacts, through: :contact_kinds
 end
